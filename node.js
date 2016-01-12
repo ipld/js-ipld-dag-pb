fab9270bb283d4a1d5bf89d6a8bcfd38d00da764
Vijayee Kulkaa <vijayee.kulkaa@hushmail.com>
2016-01-12 17:07:46 -0500

    Implements most of Node and Link functionalitry

--- node.js
+++ node.js
@@ -12,44 +12,120 @@ var Node=function() {
     var Data;
     var Cached;
     var Encoded;
+    //sort links by their name field
     var linkSort= function(a,b){return a.Name().toString('hex').localeCompare( b.Name().toString('hex'));};
+
+    this.Data= function(){
+        if(arguments.length == 0){
+            return Data;
+        }else{
+            if (arguments[0] instanceof Buffer){
+                Data= arguments[0];
+            }
+            return this;
+        }
+    }
+
+    //link to another node
+    var MakeLink = function (node){
+        var size = node.Size();
+        var mh = node.MultiHash();
+        if(!(size && mh)){
+           return null;
+        }
+        return new Link(null,size,mh)
+    };
+
+    // AddNodeLink adds a link to another node.
     this.AddNodeLink = function (name, node) {
-
+        if( typeof name != "string" || !(node instanceof Node)){
+            return
+        }
+        var link= MakeLink(node);
+        if(!link){
+            return
+        }
+        link.Name(name);
+        link.Node(node);
+        this.AddRawLink(name, link);
     };
+
+    // AddRawLink adds a copy of a link to this node
+    this.AddRawLink= function(name, link){
+        if( typeof name != "string" || !(link instanceof Link)){
+            return
+        }
+        Encoded= null;
+        Links.push(new Link(link.Name(), link.Size(), link.Hash(), link.Node()));
+    };
+
+    // AddNodeLinkClean adds a link to another node. without keeping a reference to
+    // the child node
+    this.AddNodeLinkClean= function(name, node){
+        if( typeof name != "string" || !(link instanceof Link)){
+            return
+        }
+        var link= MakeLink(node);
+        if(!link){
+            return
+        }
+
+        Encoded= null;
+        link.Name(name);
+        this.AddRawLink(name, link);
+    };
+
+    this.MultiHash=function(){
+        this.Encoded();
+        return Cached;
+    };
+
+    // Size returns the total size of the data addressed by node,
+    // including the total sizes of references.
     this.Size = function () {
-
-    };
-
-    this.Encoded = function () {
-        if (arguments.length == 0) {
-
-
-            return Encoded;
-        } else {
-            if (arguments[0] instanceof Buffer) {
-                Encoded = arguments[0];
-            }
-            return this;
+        var buf= this.Encoded();
+        if(!buf){
+            return 0;
         }
-    };
-
-    this.Cached = function () {
-        if (arguments.length == 0) {
-            return Cached;
-        } else {
-            if (arguments[0] instanceof Buffer) {
-                Cached = arguments[0];
-            }
-            return this;
-
+        size =buf.length;
+        for(link in Links){
+            size += link.Size();
         }
+        return size;
     };
-    //Encode into Protobuf
+
+    // Encoded returns the encoded raw data version of a Node instance.
+    // It may use a cached encoded version, unless the force flag is given.
+    this.Encoded = function (force) {
+        if (force || !Encoded) {
+            Encoded = this.Marshal();
+
+            if (Encoded) {
+                Cached = util.hash(Encoded);
+            }
+        }
+        return Encoded;
+    };
+
+    //Encode into a Protobuf
     this.Marshal = function(){
       var pbn = getPBNode();
       var data = merklepb.PBNode.encode(pbn);
       return data;
     };
+
+    //Decode from a Protobuf
+    this.UnMarshal = function(data){
+        var pbn = merklepb.PBNode.encode(data);
+        for(link in pbn.Links){
+           var lnk = new Link(link.Name, link.Tsize, link.Hash);
+           Links.push(lnk);
+        }
+        Links.sort(linkSort);
+        Data = pbn.Data;
+
+    };
+
     //Helper method to get a protobuf object equivalent
     var getPBNode = function(){
         var pbn = new Object();
@@ -79,8 +155,6 @@ var Link = function(name, size, hash, node){
     var Size;
     var Hash;
     var Node;
-    var Encoded;
-    var Cached;
     this.Name= function(){
         if(arguments.length == 0){
             return Name;
