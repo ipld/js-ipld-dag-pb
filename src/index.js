8f86982b540f46c54eb5ccded2f2172e4ac883ef
David Dias <daviddias.p@gmail.com>
2016-01-29 06:42:40 +0000

    refactor the code, return erros, move batch outside, prepare for factororization

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ exports.Block = require('./block.js')
 exports.DAGNode = require('./dag-node.js').DAGNode
 exports.DAGLink = require('./dag-node.js').DAGLink
 exports.DAGService = require('./dag-service.js')
+exports.Batch = require('./dag-service-batch.js')
 exports.util = require('./util.js')
