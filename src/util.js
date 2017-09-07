5412786f2149ae2ffb2bdd77dfad4e15fc66223d
dignifiedquire <dignifiedquire@gmail.com>
2017-09-07 10:33:51 +0200

    feat: replace protocol-buffers with protons
    Ref https://github.com/ipfs/js-ipfs/issues/991

--- src/util.js
+++ src/util.js
@@ -1,7 +1,8 @@
 'use strict'
 
 const CID = require('cids')
-const proto = require('./dag.proto.js')
+const protons = require('protons')
+const proto = protons(require('./dag.proto.js'))
 const DAGLink = require('./dag-link')
 const DAGNode = require('./dag-node')
 
@@ -18,21 +19,19 @@ function serialize (node, callback) {
   let serialized
 
   try {
-    const msg = proto.PBNode.create(toProtoBuf(node))
-    serialized = proto.PBNode.encode(msg).finish()
+    serialized = proto.PBNode.encode(toProtoBuf(node))
   } catch (err) {
     return callback(err)
   }
 
-  callback(null, Buffer.from(serialized))
+  callback(null, serialized)
 }
 
 function deserialize (data, callback) {
   const pbn = proto.PBNode.decode(data)
 
   const links = pbn.Links.map((link) => {
-    const size = typeof link.Tsize.toNumber === 'function' ? link.Tsize.toNumber() : link.Tsize
-    return new DAGLink(link.Name, size, Buffer.from(link.Hash))
+    return new DAGLink(link.Name, link.Tsize, link.Hash)
   })
 
   const buf = pbn.Data == null ? Buffer.alloc(0) : Buffer.from(pbn.Data)
