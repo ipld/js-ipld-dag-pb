f054ba7e92d020aca148fdeca4d341500af00979
Hugo Dias <hugomrdias@gmail.com>
2020-04-03 11:05:11 +0100

    fix: native sort is stable now

--- src/dag-node/sortLinks.js
+++ src/dag-node/sortLinks.js
@@ -1,14 +1,18 @@
 'use strict'
 
-const sort = require('stable')
 const { Buffer } = require('buffer')
 
 const linkSort = (a, b) => {
   return Buffer.compare(a.nameAsBuffer, b.nameAsBuffer)
 }
 
+/**
+ *
+ * @param {Array} links
+ * @returns {Array}
+ */
 const sortLinks = (links) => {
-  return sort(links, linkSort)
+  return links.sort(linkSort)
 }
 
 module.exports = sortLinks
