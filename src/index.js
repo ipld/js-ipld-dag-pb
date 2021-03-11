ef4cf0cb725e7fd3cf4bcc7163005b06a5b8845e
Alex Potsides <alex@achingbrain.net>
2021-03-11 12:50:27 +0000

    chore: add interface type (#191)

--- src/index.js
+++ src/index.js
@@ -7,8 +7,13 @@ const DAGLink = require('./dag-link/dagLink')
 
 /**
  * @typedef {import('./types').DAGLinkLike} DAGLinkLike
+ * @typedef {import('./types').DAGNodeLike} DAGNodeLike
+ * @typedef {import('interface-ipld-format').Format<DAGNode>} DAGNodeFormat
  */
 
+/**
+ * @type {DAGNodeFormat & { DAGNode: DAGNode, DAGLink: DAGLink }}
+ */
 module.exports = {
   DAGNode,
   DAGLink,
